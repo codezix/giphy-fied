786340ac26066c44607b251263d226028401ed40
codezix <49477576+codezix@users.noreply.github.com>
2019-10-24 02:22:47 +0100

    Update giphy.js

--- giphy.js
+++ giphy.js
@@ -1,10 +1,10 @@
 $(function() { 
 
-    
+    // Set cursor on the search bar on page load
     $('#inlineFormInputGroup').focus();
   
-    
-    var api = 'https://api.giphy.com/v1/gifs/search?q=';
+    //Create the API URL from the user inputs, API Key and Pagination
+    var api = "'https://api.giphy.com/v1/gifs/search?q=';
     var query = 'Chicago';
     var apiKey = '&api_key=AkY7KgOwfyHMbNIqwlDTLDh6lIgZhWK7';
     var lang = '&lang=es';
@@ -14,7 +14,7 @@ $(function() {
   
     $('#trending').click(function() {
       var url = 'https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC';
-      var flag = 1; 
+      var flag = 1; //trending search flag
     var fetchData = $.getJSON(url, gotData);
     });
   
@@ -23,6 +23,7 @@ $(function() {
       query = $('#inlineFormInputGroup').val();
       rating = $( "#rating option:selected" ).val();
       
+      //Display error if no input entered
       if((query.length==0) && (flag == 0)) { 
         $(".form-inline").effect("shake");
         $("#inlineFormInputGroup").addClass("error");
@@ -31,32 +32,32 @@ $(function() {
       else {
         $("#inlineFormInputGroup").removeClass("error");
         
-        
+        //Generate entire URL from the user input
         var url = api+query+apiKey+lang+'&limit='+apiPagination+rating;
         var fetchData = $.getJSON(url, gotData);
         
-      }
-    });
+      }// end check if valid input
+    });//end submit click event
   
-   
+    //Display more results as per request
     $('#load-more').click(function(){ 
       apiPagination+=10;
       $("#submit").click();
     });
   
-  });
+  });//document ready function end
   
   
   
   function gotData(data){
-    
+    //continue if API exists
     if(data.meta.status){ 
       if(data.data.length > 0) {
   
         $(".gif-results").show();
         $('.no-result').hide();
   
-        
+        //Remove any existing data - used to clearing out previous searches
         if($("div.gif-results img").length > 0) {
           $("div.gif-results img").remove();
         } 
@@ -65,24 +66,24 @@ $(function() {
         $('.loading-gif').css("display", "block");
         
         
-
+        //Iterate through all of the returned JSON data and append to HTML 
         for(var i=0; i<data.data.length; i++) {
   
-          
+          //Get the URL from JSON
           var gifURL = data.data[i].images.original.url;
-         
+          // var giphyURL = data.data[i].url;
   
-         
+          // $(".insert-gif").attr('src', gifURL);
           var img = $('<img class="gifs">'); 
           img.attr('src', gifURL);
           img.appendTo('.gif-results');
   
-        }
+        }//end for loop
   
         $('.loading-gif').css("display", "none");
   
-        $("#load-more").show(); 
-        $(".default-gif").hide();
+        $("#load-more").show(); //show load more button at the end of the list
+        $(".default-gif").hide(); //Hide Placeholder GIF when searched
   
       }
       else {
@@ -90,11 +91,12 @@ $(function() {
         $("#load-more").hide();
         $('.no-result').show();
       }
-    } 
+    }//end data.meta.status 
    
-  }
+  }// end function gotData
   
 
+    //Blur out of the search bar after enter key hit
     $('#inlineFormInputGroup').blur(); 
    
   
